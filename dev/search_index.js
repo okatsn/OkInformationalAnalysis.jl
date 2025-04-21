19580736a4be1780c61582454300dfbd4cfd6e3f
Documenter.jl <documenter@juliadocs.github.io>
2025-04-21 03:12:56 +0000

    build based on 6473c7d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkInformationalAnalysis","category":"page"},{"location":"#OkInformationalAnalysis","page":"Home","title":"OkInformationalAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkInformationalAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkInformationalAnalysis]","category":"page"}]
+}
